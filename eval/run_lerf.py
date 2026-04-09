874fb4c549fb2cdebdb2a7c0846c60a830d239f1
Li-Kunyi <likunyi0823@gmail.com>
2026-04-09 13:29:55 +0200

    refine preprocess: feature for all level

--- eval/run_lerf.py
+++ eval/run_lerf.py
@@ -80,6 +80,9 @@ def generate(dataset, opt, pipeline, gaussian_ckpt_path, attn_ckpt_path, scene_n
 
         (model_params, first_iter) = torch.load(f"{gaussian_ckpt_path}/gaussians.pth")
         gaussians.restore_feature(model_params, opt)
+        if opt.use_mlp:
+            gaussians.set_mlp(opt.ins_feature_dim)
+            gaussians.load_mlp(gaussian_ckpt_path)
 
         background = torch.tensor([1,1,1], dtype=torch.float32, device="cuda")
         views = scene.getTrainCameras()
@@ -91,9 +94,6 @@ def generate(dataset, opt, pipeline, gaussian_ckpt_path, attn_ckpt_path, scene_n
 
         Attn = Attention(feat_dim=opt.ins_feature_dim,
                          vl_feat_dim=opt.vl_feature_dim, 
-                         num_slots=opt.slot_num, 
-                         app_slot_dim=opt.app_slot_dim, 
-                         vl_slot_dim=opt.vl_slot_dim,
                          use_geo=use_geo,
                          use_rgb=use_rgb,
                          use_ins=use_ins
