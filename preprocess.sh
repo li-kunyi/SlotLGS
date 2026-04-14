113c4459e7728c3b28d60d4a9f03d068bd351ef8
Li-Kunyi <likunyi0823@gmail.com>
2026-04-14 12:16:33 +0200

    add eval mipnerf360

--- preprocess.sh
+++ preprocess.sh
@@ -3,11 +3,11 @@
 # python preprocessor/run.py --dataset_path /home/kunyi/work/data/lerf_ovs/teatime --sam_ckpt_path ckpt/sam_vit_h_4b8939.pth --encoder clip --empty_bg
 # python preprocessor/run.py --dataset_path /home/kunyi/work/data/lerf_ovs/waldo_kitchen --sam_ckpt_path ckpt/sam_vit_h_4b8939.pth --encoder clip --empty_bg
 
-python preprocessor/run.py --dataset_path /home/kunyi/work/data/scannet_opengs/scene0000_00 --sam_ckpt_path ckpt/sam_vit_h_4b8939.pth --encoder clip --empty_bg
-python preprocessor/run.py --dataset_path /home/kunyi/work/data/scannet_opengs/scene0062_00 --sam_ckpt_path ckpt/sam_vit_h_4b8939.pth --encoder clip --empty_bg
-python preprocessor/run.py --dataset_path /home/kunyi/work/data/scannet_opengs/scene0070_00 --sam_ckpt_path ckpt/sam_vit_h_4b8939.pth --encoder clip --empty_bg
-python preprocessor/run.py --dataset_path /home/kunyi/work/data/scannet_opengs/scene0097_00 --sam_ckpt_path ckpt/sam_vit_h_4b8939.pth --encoder clip --empty_bg
-python preprocessor/run.py --dataset_path /home/kunyi/work/data/scannet_opengs/scene0140_00 --sam_ckpt_path ckpt/sam_vit_h_4b8939.pth --encoder clip --empty_bg
-python preprocessor/run.py --dataset_path /home/kunyi/work/data/scannet_opengs/scene0347_00 --sam_ckpt_path ckpt/sam_vit_h_4b8939.pth --encoder clip --empty_bg
-python preprocessor/run.py --dataset_path /home/kunyi/work/data/scannet_opengs/scene0590_00 --sam_ckpt_path ckpt/sam_vit_h_4b8939.pth --encoder clip --empty_bg
-python preprocessor/run.py --dataset_path /home/kunyi/work/data/scannet_opengs/scene0645_00 --sam_ckpt_path ckpt/sam_vit_h_4b8939.pth --encoder clip --empty_bg
+python preprocessor/run.py --dataset_path /home/kunyi/work/data/scannet_ovs/scene0000_00 --sam_ckpt_path ckpt/sam_vit_h_4b8939.pth --encoder clip --empty_bg
+python preprocessor/run.py --dataset_path /home/kunyi/work/data/scannet_ovs/scene0062_00 --sam_ckpt_path ckpt/sam_vit_h_4b8939.pth --encoder clip --empty_bg
+python preprocessor/run.py --dataset_path /home/kunyi/work/data/scannet_ovs/scene0070_00 --sam_ckpt_path ckpt/sam_vit_h_4b8939.pth --encoder clip --empty_bg
+python preprocessor/run.py --dataset_path /home/kunyi/work/data/scannet_ovs/scene0097_00 --sam_ckpt_path ckpt/sam_vit_h_4b8939.pth --encoder clip --empty_bg
+python preprocessor/run.py --dataset_path /home/kunyi/work/data/scannet_ovs/scene0140_00 --sam_ckpt_path ckpt/sam_vit_h_4b8939.pth --encoder clip --empty_bg
+python preprocessor/run.py --dataset_path /home/kunyi/work/data/scannet_ovs/scene0347_00 --sam_ckpt_path ckpt/sam_vit_h_4b8939.pth --encoder clip --empty_bg
+python preprocessor/run.py --dataset_path /home/kunyi/work/data/scannet_ovs/scene0590_00 --sam_ckpt_path ckpt/sam_vit_h_4b8939.pth --encoder clip --empty_bg
+python preprocessor/run.py --dataset_path /home/kunyi/work/data/scannet_ovs/scene0645_00 --sam_ckpt_path ckpt/sam_vit_h_4b8939.pth --encoder clip --empty_bg
