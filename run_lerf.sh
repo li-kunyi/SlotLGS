8f9d17ccc0e118577890c559602dc278421f56bb
li-kunyi <likunyi0823@gmail.com>
2026-04-15 03:13:06 +0800

    none

--- run_lerf.sh
+++ run_lerf.sh
@@ -1,35 +1,34 @@
 DATASET_PATH="/root/autodl-fs/lerf_ovs"
-SAVE_PATH="output/lerf_ovs_0412_mlp"
+SAVE_PATH="output/lerf_ovs_0414_mlp"
 ENCODER="clip"
 LEVELS=(
   "l"
   "m"
   "s"
-  # "ins"
   )
 
 SCENES=(
-  # "figurines" 
+  "figurines" 
   "ramen" 
-  # "teatime" 
-  # "waldo_kitchen"
+  "teatime" 
+  "waldo_kitchen"
   )
 
 for SCENE_NAME in "${SCENES[@]}"
 do
   echo "Processing scene: $SCENE_NAME"
 
-  # for LEVEL in "${LEVELS[@]}"
-  # do
-  #   echo "Training with level: $LEVEL"
+  for LEVEL in "${LEVELS[@]}"
+  do
+    echo "Training with level: $LEVEL"
 
-  #   python train.py \
-  #     -s $DATASET_PATH/$SCENE_NAME \
-  #     -m $SAVE_PATH/$SCENE_NAME \
-  #     --encoder $ENCODER \
-  #     --level $LEVEL \
-  #     --ckpt_path $SAVE_PATH/$SCENE_NAME
-  # done
+    python train.py \
+      -s $DATASET_PATH/$SCENE_NAME \
+      -m $SAVE_PATH/$SCENE_NAME \
+      --encoder $ENCODER \
+      --level $LEVEL \
+      --ckpt_path $SAVE_PATH/$SCENE_NAME
+  done
 
   python -m eval.run_lerf_langsplat \
     -s $DATASET_PATH/$SCENE_NAME \
@@ -41,14 +40,14 @@ do
     --json_dir $DATASET_PATH/label \
     --level all
 
-  # python -m eval.run_lerf_3d_langsplat \
-  #   -s $DATASET_PATH/$SCENE_NAME \
-  #   -m $SAVE_PATH/$SCENE_NAME \
-  #   --encoder $ENCODER \
-  #   --scene_name $SCENE_NAME \
-  #   --gaussian_ckpt $SAVE_PATH/$SCENE_NAME \
-  #   --attn_ckpt $SAVE_PATH/$SCENE_NAME \
-  #   --json_dir $DATASET_PATH/label \
-  #   --level all
+  python -m eval.run_lerf_3d_langsplat \
+    -s $DATASET_PATH/$SCENE_NAME \
+    -m $SAVE_PATH/$SCENE_NAME \
+    --encoder $ENCODER \
+    --scene_name $SCENE_NAME \
+    --gaussian_ckpt $SAVE_PATH/$SCENE_NAME \
+    --attn_ckpt $SAVE_PATH/$SCENE_NAME \
+    --json_dir $DATASET_PATH/label \
+    --level all
 
 done
